1ad96ec18722b2c95317ce30052e6c9ebdaa3417
penpeng <2072117261@qq.com>
2022-05-14 00:48:38 +0800

    orgin fileServer

---
+++ StaticFileServer/StaticFileServer/Controllers/ImageFilesController.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace StaticFileServer.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ImageFilesController : ControllerBase
+    {
+        private IWebHostEnvironment _env;
+        public ImageFilesController(IWebHostEnvironment env)
+        {
+            _env = env;
+        }
+
+        // GET api/<ImageFilesController>/5
+        [HttpGet("{imageName}")]
+        public IActionResult Get(string imageName)
+        {
+            var filePath = Path.Combine(
+                _env.ContentRootPath, "MyStaticFiles", "images", imageName);
+
+            if (System.IO.File.Exists(filePath))
+            {
+                return (PhysicalFile(filePath, "image/jpeg"));
+            }
+
+            return NotFound();
+        }
+
+        // PUT api/<ImageFilesController>/5
+        [HttpPut("{imageName}")]
+        //[Consumes("multipart/form-data")]
+        async public Task<IActionResult> Put(string imageName, [FromForm] IFormFile formFile)
+        {
+            var filePath = Path.Combine(
+            _env.ContentRootPath, "MyStaticFiles", "images", imageName);
+
+            if (System.IO.File.Exists(filePath))
+            {
+                System.IO.File.Delete(filePath);
+            }
+
+            using (var stream = System.IO.File.Create(filePath))
+            {
+                await formFile.CopyToAsync(stream);
+            }
+
+            return Ok();
+        }
+
+        // DELETE api/<ImageFilesController>/5
+        [HttpDelete("{imageName}")]
+        public void Delete(string imageName)
+        {
+            var filePath = Path.Combine(
+            _env.ContentRootPath, "MyStaticFiles", "images", imageName);
+
+            if (System.IO.File.Exists(filePath))
+            {
+                System.IO.File.Delete(filePath);
+            }
+
+        }
+    }
+}
---
+++ StaticFileServer/StaticFileServer/Startup.cs
@@ -0,0 +1,61 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.FileProviders;
+using Microsoft.Extensions.Hosting;
+using System.IO;
+using System.Linq;
+
+namespace StaticFileServer
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+            services.AddControllers();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            const string cacheMaxAge = "604800";
+
+            app.UseStaticFiles(new StaticFileOptions
+            {
+                FileProvider = new PhysicalFileProvider(
+                    Path.Combine(env.ContentRootPath, "MyStaticFiles")),
+                RequestPath = "/StaticFiles",
+                OnPrepareResponse = ctx =>
+                 {
+                     ctx.Context.Response.Headers
+                     .Append("Cache-Control", $"public,max-age={cacheMaxAge}");
+                 }
+            });
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
